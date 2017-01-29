c245d7b6cb4a817a7a822be9cbee65d402802a2e
Christer Magerøy Dahlslett <christermageroydahlslett@Mr-Christer-Design.local>
2017-01-29 15:43:20 +0100

    first commit

--- web.py
+++ web.py
@@ -1,34 +1,34 @@
-# How to import the weather app we made previous: 
-# in the html we change name to adress
-# in this file we want to return the forecast - not adress. 
-#so in the index function we run the functiion from weather.py called "get_location()"
-#We pass through the "adress" which the user will fill in on the line above, which is linked
-#to the html document. 
-
-# This works but it returns the adress as "none", so we added a if statement so it would only
-# print out the forecast if someone had put in their adress
-# we also had to make forecast = none, because the return needed the forecast to run
+# Importing libaries. 
 
 from flask import Flask, render_template, request
-import weater
+import yelp_script
 import os
+
+# And crating an instance of this class. 
 app = Flask(__name__)
+
+# The route() decorator tells Flask what URL should trigger our function.
+# We define a varaibel called location, which requests the location from the user.
+# Location is the argument our yelp_script.py function need to run. 
+# We use an if-statement so we can show the recommendation only when a location is given.
+# This is done by running the function from our yelp_script.py with the location requested earlier. 
+# if there is no input of location there will be no recommendation given. 
+# Then we return this to the index.html page, and we define what this should be called in the html document
+# so we can call upon it there. 
+
+# Last we have an if statement that runs the script. 
 
 @app.route("/")
 def index():
-    adress = request.values.get("adress")
-    forecast = None
-    if adress:   
-        forecast = weater.get_location(adress)
-    return render_template("index.html", forecast=forecast)
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
+    location = request.values.get("location")
+    if location:
+        recommendation = yelp_script.get_businesses(location)
+    else:
+        recommendation = None
+    return render_template("index.html", recommendation=recommendation)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
 
 
 
